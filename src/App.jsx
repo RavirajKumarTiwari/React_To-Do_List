a0aff09c2b7125e969a7c3ec5b019bbb542e16ad
Raviraj Kumar <ravirajk545@gmail.com>
2023-10-29 19:05:58 +0530

    bg change

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,8 @@ function App() {
         <TodoProvider
             value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
         >
-            <div className="bg-[#172842] h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+            <div className="bg-slate-800 h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto bg-black/30 shadow-lg rounded-lg backdrop-blur-sm px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">
                         Manage Your Todos
                     </h1>
